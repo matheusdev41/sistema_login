de563acc99963414c6dcb9b61c1db3929db0dc8d
Matheus Pessoa <matheus41.dev@gmail.com>
2024-07-01 05:46:42 -0300

    adição de painel.php e protect.php + modificação dos arquivos conexao.php e index.php

--- conexao.php
+++ conexao.php
@@ -5,7 +5,7 @@ $senha = '';
 $database = 'login';
 $host = 'localhost';
 
-$mysqli = new mysqli($host, $usuario, $senha, $database );
+$mysqli = new mysqli($host, $usuario, $senha, $database);
 
 if ($mysqli -> error){
     die("Falha ao conectar ao banco de dados " . $mysqli->error);
--- index.php
+++ index.php
@@ -2,20 +2,50 @@
 include('conexao.php');
 
 //isset = "se existir" (verificando a existência do login e senha no banco) 
-if(isset($_POST['email']) || isset($_POST['senha'])){
+if(isset($_POST['email']) || isset($_POST['senha'])) {
 
     //strlen é a quantidade de caracteres
     //se a quantidade de caracteres do email for zero..
-    if(strlen($_POST['email']) == 0) {
+    if(strlen($_POST['email']) == null) {
         echo "Preencha seu e-mail";
 
     //se não for zero e a senha for zero...
-    }else if(strlen($_POST['senha']) == 0) {
+    } else if(strlen($_POST['senha']) == null) {
         echo "Preencha sua senha";
 
     //se não, processo verificação
-    }else {
+    } else {
 
+        // real_escape_string para limpar o campo de email
+        // variável mysqli vem do arquivo de conexão
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die('Falha na execução do codigo SQL: ' . $mysqli->error);
+
+        //autenticação
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1){
+            
+            $usuario = $sql_query->fetch_assoc();
+            
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+
+            //inicio de sessão 
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            //redirecionamento da pagina
+            header("Location: painel.php");
+
+        } else {
+           echo "Falha ao logar! E-mail ou senha incorretos";
+        }
+        
     }
 }
 ?>
@@ -30,7 +60,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])){
 <body>
     <h1>Acesse sua conta</h1>
 <!--form -->
-    <form action="" method="$_POST">
+    <form action="" method="POST">
         <p>
             <label>E-mail</label>
             <input type="text" name="email">
@@ -41,6 +71,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])){
         </p>
         <p>
             <button type="submit">Entrar</button>
+        </p>
     </form>
 <!--end form-->
 </body>
